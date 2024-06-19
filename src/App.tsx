d1636e58a309e22c6c37ee256219997ecd649fdd
Marius Hartmann <marius8hartmann@gmail.com>
2024-06-19 16:16:51 +0200

    better imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import "./App.css";
+import { Map } from "./components/map/map";
 
 export const App = () => {
-  return <div className="App">APP</div>;
+  return (
+    <div className="App">
+      APP
+      <Map></Map>
+    </div>
+  );
 };
